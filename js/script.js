eb620b9b5a502d3cdbd9f74599e4eda8de8fb4f9
Amir <seyyeddev@gmail.com>
2023-06-06 20:56:57 +0330

    make responsive with no grid

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-// To prevent default bahavior button
-const submit = document.querySelector(".content__btn");
+let submit = document.querySelector(".content__btn");
+let image = document.querySelector(".image__container img");
 
+
+// To prevent default bahavior button
 submit.addEventListener("click", (e) => {
     e.preventDefault();
 })
+
+//change image
+if (window.innerWidth > "1024") {
+    image.setAttribute("src", "./images/hero-desktop.jpg")
+}
